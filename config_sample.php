793e03c323809d343ebb2e663ff5807d1ea1cf09
rolandbaer <rbaer@gmx.ch>
2023-06-09 22:51:17 +0200

    added configuration sample

---
+++ config_sample.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+    array("name"=>"page_one",    "path"=>"/folder/to/page/one"),
+    array("name"=>"page_two",    "path"=>"/folder/to/page/two"),
+    array("name"=>"page_three",  "path"=>"/folder/to/page/three"),
+    array("name"=>"app_four",    "path"=>"/other/folder/to/app/four"),
+)
+?>
