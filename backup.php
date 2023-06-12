3f79f6a277e06d9c059d8d11593af27589c9c778
rolandbaer <rbaer@gmx.ch>
2023-06-12 20:27:39 +0200

    reload, beautify
    added reload button, beautified the design

--- backup.php
+++ backup.php
@@ -36,26 +36,60 @@ if(is_numeric($run) and (int)$run < count($config)) {
 
 ?>
 <!DOCTYPE html>
-<html>
-<head><title>Web Backup</title></head>
+<html><head><title>Web Backup</title>
+<style>
+body {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  padding: 1em;
+}
+p.success {
+  background-color: LawnGreen;
+  padding: 1em;
+}
+.backups {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+}
+.backups td, .backups th {
+  border: 1px solid #ddd;
+  padding: 12px 16px;
+}
+.backups tr:nth-child(even) { background-color: #f2f2f2; }
+.backups tr:hover { background-color: #ddd; }
+.backups th {
+  text-align: left;
+  background-color: navy;
+  color: white;
+}
+.btn {
+  border-radius: 5px;
+  color: white;
+  background-color: darkorchid;
+  padding: 6px 18px;
+  text-decoration: none;
+}
+</style></head>
 <body>
+<h1>Web Backup</h1>
 <?php
 if($backuped != "") {
-    echo('<div><p style="background-color: LawnGreen">' . $backuped . ' backed up.</p></div>');
+    echo('<div><p class="success"><b>' . $backuped . '</b> backed up.</p></div>');
 }
-echo("<table><tr><th>Name</th><th>Job</th><th>Date/Time</th><th>Size</th><th>Download</th></tr>\n");
+echo('<table class="backups"><tr><th>Name</th><th>Job</th><th>Date/Time</th><th>Size</th><th>Download</th></tr>');
+echo("\n");
 for($row = 0; $row < count($config); $row++) {
     $name = $config[$row]["name"];
-    echo('<tr><td>' . $name . '</td><td><a href="backup.php?run=' . $row . '">Run</a></td>');
+    echo('<tr><td>' . $name . '</td><td><a class="btn" href="backup.php?run=' . $row . '">Run</a></td>');
     $filename = 'backup_' . $name .'.zip';
     echo('<td>' . get_timestanp($filename) . '</td>');
     echo('<td>' . get_size($filename) . '</td><td>');
     if(file_exists($filename)) {
-        echo('<a href="' . $filename .'">download</a>');
+        echo('<a class="btn" href="' . $filename .'">Download</a>');
     }
     echo("</td></tr>\n");
 }
 echo('</table>');
 ?>
+<div><br /><a class="btn" href="backup.php">Reload</a></div>
 </body>
 </html>
